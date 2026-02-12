a32f514883cb2504e606ac3c28d212b8a85d0934
jeri <jerimonski@gmail.com>
2026-02-12 15:56:55 -0300

    base

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './modules/products/products.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { PaymentsModule } from './modules/payments/payments.module';
+import { NewsletterModule } from './modules/newsletter/newsletter.module';
+
+@Module({
+  imports: [ProductsModule, CategoriesModule, OrdersModule, PaymentsModule, NewsletterModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
