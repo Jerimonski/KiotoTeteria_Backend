d1eb2a61b5ee02b868c9882f624af3f7fd75ad34
jeri <jerimonski@gmail.com>
2026-02-20 19:52:55 -0300

    feat: category endpoint/rutes defined

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private categoriesService: CategoriesService) {}
+  @Post()
+  createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.categoriesService.create(createCategoryDto);
+  }
+  @Get()
+  findAll() {
+    return this.categoriesService.findAll();
+  }
+
+  @Get(':identifier')
+  findOne(@Param('identifier') identifier: string) {
+    return this.categoriesService.findOne(identifier);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return this.categoriesService.update(id, updateCategoryDto);
+  }
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.remove(id);
+  }
+}
