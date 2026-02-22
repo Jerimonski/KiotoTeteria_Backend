48e1ad579e53adc8b9759bc3a923787631f8ede2
jeri <jerimonski@gmail.com>
2026-02-21 22:13:42 -0300

    feat: controller routes established

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+  @Get('paginate')
+  paginate(
+    @Query('page', ParseIntPipe) page: number,
+    @Query('pageSize', ParseIntPipe) pageSize: number,
+  ) {
+    return this.productsService.paginate(page, pageSize);
+  }
+  @Get('category/:categoryId')
+  findByCategory(@Param('categoryId', ParseIntPipe) categoryId: number) {
+    return this.productsService.findByCategory(categoryId);
+  }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+}
