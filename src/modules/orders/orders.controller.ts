9ef25f25e06d9c0359bfab793aafee2c3ca50058
jeri <jerimonski@gmail.com>
2026-02-23 19:07:10 -0300

    feat: service ready and endpoint established

--- src/modules/orders/orders.controller.ts
+++ src/modules/orders/orders.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  ParseIntPipe,
+  Query,
+  Param,
+  Post,
+  Body,
+  Patch,
+} from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { OrderStatus } from '@prisma/client';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderStatusDto } from './dto/update-order.dto';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersService.create(createOrderDto);
+  }
+  @Patch(':id/status')
+  updateStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateOrderStatusDto,
+  ) {
+    return this.ordersService.updateStatus(id, dto.status);
+  }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.ordersService.findOne(id);
+  }
+  @Get()
+  findAll(
+    @Query('page', ParseIntPipe) page = 1,
+    @Query('pageSize', ParseIntPipe) pageSize = 10,
+    @Query('status') status?: OrderStatus,
+    @Query('customerEmail') customerEmail?: string,
+  ) {
+    return this.ordersService.findAll({
+      page,
+      pageSize,
+      status,
+      customerEmail,
+    });
+  }
+}
--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -1,4 +1,92 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { Order, OrderStatus, Prisma } from '@prisma/client';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { PaginationOptions } from './../../common/pagination/pagination-options';
+
+interface FindAllParams {
+  page: number;
+  pageSize: number;
+  status?: OrderStatus;
+  customerEmail?: string;
+}
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createOrderDto: CreateOrderDto): Promise<Order> {
+    try {
+      return await this.prisma.order.create({
+        data: {
+          customerEmail: createOrderDto.customerEmail,
+          status: OrderStatus.PENDING,
+          totalAmount: createOrderDto.totalAmount,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new BadRequestException('Category name or slug already exists');
+      }
+      throw error;
+    }
+  }
+  async updateStatus(id: number, status: OrderStatus) {
+    const order = await this.prisma.order.findUnique({
+      where: { id },
+    });
+
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
+
+    return this.prisma.order.update({
+      where: { id },
+      data: { status },
+    });
+  }
+  async findOne(id: number): Promise<Order> {
+    const order = await this.prisma.order.findUnique({
+      where: { id },
+    });
+
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
+    return order;
+  }
+  async findAll(params: FindAllParams) {
+    const { page, pageSize, skip, take } = PaginationOptions.resolve(params);
+
+    const { status, customerEmail } = params;
+
+    const where = {
+      ...(status && { status }),
+      ...(customerEmail && { customerEmail }),
+    };
+
+    const [orders, total] = await this.prisma.$transaction([
+      this.prisma.order.findMany({
+        where,
+        skip,
+        take,
+        orderBy: {
+          createdAt: 'desc',
+        },
+      }),
+      this.prisma.order.count({ where }),
+    ]);
+
+    return {
+      data: orders,
+      meta: PaginationOptions.buildMeta(total, page, pageSize),
+    };
+  }
+}
