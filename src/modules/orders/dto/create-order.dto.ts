0e802f7639fb4f98b5c4f1731e7354835e04b3e3
jeri <jerimonski@gmail.com>
2026-02-27 19:43:38 -0300

    feat: updating dto

--- src/modules/orders/dto/create-order.dto.ts
+++ src/modules/orders/dto/create-order.dto.ts
@@ -1,12 +1,32 @@
-import { IsNotEmpty, IsNumber, IsEmail, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsEmail,
+  Min,
+  IsInt,
+  IsArray,
+  ArrayMinSize,
+  ValidateNested,
+} from 'class-validator';
+
+class CreateOrderItemDto {
+  @IsInt()
+  @Min(1)
+  productId!: number;
+
+  @IsInt()
+  @Min(1)
+  quantity!: number;
+}
 
 export class CreateOrderDto {
   @IsEmail()
   @IsNotEmpty()
-  customerEmail: string;
+  customerEmail!: string;
 
-  @IsNumber()
-  @Min(0)
-  @IsNotEmpty()
-  totalAmount: number;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => CreateOrderItemDto)
+  items!: CreateOrderItemDto[];
 }
