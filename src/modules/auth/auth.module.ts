4cf5bd740b3b74f68c855cd53def56b8dda2d407
jeri <jerimonski@gmail.com>
2026-03-03 22:10:10 -0300

    fix:  replace register for registerAsync

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,15 +2,17 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from 'src/common/jwt.strategy';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     PrismaModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'supersecret',
-      signOptions: { expiresIn: '1h' },
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '3h' },
+      }),
     }),
   ],
   controllers: [AuthController],
