d483800ab631bdfca7924ec37225484d65cf6aeb
jeri <jerimonski@gmail.com>
2026-03-09 22:29:22 -0300

    feat; newsletter  finished

--- src/modules/newsletter/newsletter.controller.ts
+++ src/modules/newsletter/newsletter.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { NewsletterService } from './newsletter.service';
+import { CreateNewsletterSubscriberDto } from './dto/create-newsletter-subscriber.dto';
 
 @Controller('newsletter')
-export class NewsletterController {}
+export class NewsletterController {
+  constructor(private readonly newsletterService: NewsletterService) {}
+
+  @Post('subscribe')
+  subscribe(@Body() createDto: CreateNewsletterSubscriberDto) {
+    return this.newsletterService.subscribe(createDto);
+  }
+}
--- src/modules/newsletter/newsletter.service.ts
+++ src/modules/newsletter/newsletter.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { CreateNewsletterSubscriberDto } from './dto/create-newsletter-subscriber.dto';
 
 @Injectable()
-export class NewsletterService {}
+export class NewsletterService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async subscribe(createDto: CreateNewsletterSubscriberDto) {
+    const { email } = createDto;
+
+    const existing = await this.prisma.newsletterSubscriber.findUnique({
+      where: { email },
+    });
+
+    if (existing) {
+      throw new ConflictException('Email already subscribed');
+    }
+
+    return this.prisma.newsletterSubscriber.create({
+      data: {
+        email,
+      },
+    });
+  }
+}
