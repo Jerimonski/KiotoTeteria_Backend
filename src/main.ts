74207bca65f27a6a2798ee06d055e5b7da4047fc
jeri <jerimonski@gmail.com>
2026-03-09 22:28:19 -0300

    feat: adding logs

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,25 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+  try {
+    const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT ?? 3000);
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
+    const port = process.env.PORT ?? 3000;
+    await app.listen(port);
+
+    console.log(`🚀 Server ready at http://localhost:${port}`);
+  } catch (err) {
+    console.error('❌ Error starting app:', err);
+    process.exit(1);
+  }
 }
+
 bootstrap();
