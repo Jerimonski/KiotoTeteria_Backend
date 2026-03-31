fc51d7044353c4f2ef1dadc629bd8ce3c0c54710
jeri <jerimonski@gmail.com>
2026-03-30 21:48:54 -0300

    feat: blocking orders routes

--- src/modules/orders/orders.controller.ts
+++ src/modules/orders/orders.controller.ts
@@ -7,11 +7,15 @@ import {
   Post,
   Body,
   Patch,
+  UseGuards,
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrderStatus } from '@prisma/client';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderStatusDto } from './dto/update-order.dto';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/guards/roles.guard';
 
 @Controller('orders')
 export class OrdersController {
@@ -20,6 +24,8 @@ export class OrdersController {
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
   }
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Patch(':id/status')
   updateStatus(
     @Param('id', ParseIntPipe) id: number,
@@ -27,10 +33,15 @@ export class OrdersController {
   ) {
     return this.ordersService.updateStatus(id, dto.status);
   }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.ordersService.findOne(id);
   }
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Get()
   findAll(
     @Query('page', ParseIntPipe) page = 1,
